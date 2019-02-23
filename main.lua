bfd55ea46cb5b9141e1c2303e4efaca1090ef6e9
Andre Duvoisin <anduv13@gmail.com>
2019-02-23 12:57:15 -0600

    Split code out into functions.

--- main.lua
+++ main.lua
@@ -1,19 +1,19 @@
-local EventFrame = CreateFrame("Frame")
 
-EventFrame:RegisterEvent("PLAYER_LOGIN")
-
-EventFrame:SetScript("OnEvent", function(self,event,...)
-    -- bottom styling
+local function RemoveBottomActionBarStyling()
     MainMenuBarArtFrame.LeftEndCap:Hide()
     MainMenuBarArtFrame.RightEndCap:Hide()
     MainMenuBarArtFrameBackground:Hide()
-    StatusTrackingBarManager:Hide()
-    ActionBarDownButton:Hide()
-    ActionBarUpButton:Hide()
-    MainMenuBarArtFrame.PageNumber:SetAlpha(0)
 
-    -- menu and bags
+    StatusTrackingBarManager:Hide()
+
+    ActionBarUpButton:Hide()
+    ActionBarDownButton:Hide()
+    MainMenuBarArtFrame.PageNumber:SetAlpha(0)
+end
+
+local function RemoveMenuMicroButtonAndBagsBar()
     MicroButtonAndBagsBar:Hide()
+
     CharacterMicroButton:Hide()
     SpellbookMicroButton:Hide()
     TalentMicroButton:Hide()
@@ -25,15 +25,30 @@ EventFrame:SetScript("OnEvent", function(self,event,...)
     EJMicroButton:Hide()
     StoreMicroButton:Hide()
     MainMenuMicroButton:Hide()
+
     -- required because the game does an extra StoreMicroButton:Show() after login
     StoreMicroButton:SetScript("OnShow", StoreMicroButton.Hide)
+end
 
-    -- remove macro names from bars
+local function RemoveMacroNamesFromActionBars()
     for i=1, 12 do
-        _G["ActionButton"..i.."Name"]:SetAlpha(0) -- main bar
-        _G["MultiBarBottomRightButton"..i.."Name"]:SetAlpha(0) -- bottom right bar
-        _G["MultiBarBottomLeftButton"..i.."Name"]:SetAlpha(0) -- bottom left bar
-        _G["MultiBarRightButton"..i.."Name"]:SetAlpha(0) -- right bar
-        _G["MultiBarLeftButton"..i.."Name"]:SetAlpha(0) -- left bar
+        -- main action bar
+        _G["ActionButton"..i.."Name"]:SetAlpha(0)
+        -- bottom right action bar
+        _G["MultiBarBottomRightButton"..i.."Name"]:SetAlpha(0)
+        -- bottom left action bar
+        _G["MultiBarBottomLeftButton"..i.."Name"]:SetAlpha(0)
+        -- right action bar
+        _G["MultiBarRightButton"..i.."Name"]:SetAlpha(0)
+        -- left action bar
+        _G["MultiBarLeftButton"..i.."Name"]:SetAlpha(0)
     end
+end
+
+local EventFrame = CreateFrame("Frame")
+EventFrame:RegisterEvent("PLAYER_LOGIN")
+EventFrame:SetScript("OnEvent", function(self, event, ...)
+    RemoveBottomActionBarStyling()
+    RemoveMenuMicroButtonAndBagsBar()
+    RemoveMacroNamesFromActionBars()
 end)
