50fde303545a0810c261ad2d79a03ee8924a7832
Andre Duvoisin <anduv13@gmail.com>
2019-02-24 11:39:30 -0600

    Finish up action bar 16 movement function.

--- main.lua
+++ main.lua
@@ -32,8 +32,8 @@ end
 
 -- bottom right and bottom left are also anchored to the main action bar
 local MAIN_ACTION_BAR_FIRST_BUTTON = "ActionButton"
-local BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON = "MultiBarBottomRightButton"
 local BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON = "MultiBarBottomLeftButton"
+local BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON = "MultiBarBottomRightButton"
 local RIGHT_ACTION_BAR_FIRST_BUTTON = "MultiBarRightButton"
 local LEFT_ACTION_BAR_FIRST_BUTTON = "MultiBarLeftButton"
 
@@ -45,32 +45,124 @@ end
 
 local function RemoveMacroNamesFromAllActionBars()
     RemoveMacroNamesFromActionBar(MAIN_ACTION_BAR_FIRST_BUTTON)
-    RemoveMacroNamesFromActionBar(BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON)
     RemoveMacroNamesFromActionBar(BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON)
+    RemoveMacroNamesFromActionBar(BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON)
     RemoveMacroNamesFromActionBar(RIGHT_ACTION_BAR_FIRST_BUTTON)
     RemoveMacroNamesFromActionBar(LEFT_ACTION_BAR_FIRST_BUTTON)
 end
 
-local function RearrangeActionBarHorizontal(name)
-    for i = 2, 12 do
-        local button = _G[name..i]
+local FIRST_ACTION_BAR_BUTTON_NAMES = {
+    MAIN_ACTION_BAR_FIRST_BUTTON..1,
+    MAIN_ACTION_BAR_FIRST_BUTTON..2,
+    MAIN_ACTION_BAR_FIRST_BUTTON..3,
+    MAIN_ACTION_BAR_FIRST_BUTTON..4,
+    MAIN_ACTION_BAR_FIRST_BUTTON..5,
+    MAIN_ACTION_BAR_FIRST_BUTTON..6,
+    MAIN_ACTION_BAR_FIRST_BUTTON..7,
+    MAIN_ACTION_BAR_FIRST_BUTTON..8,
+    MAIN_ACTION_BAR_FIRST_BUTTON..9,
+    MAIN_ACTION_BAR_FIRST_BUTTON..10,
+    MAIN_ACTION_BAR_FIRST_BUTTON..11,
+    MAIN_ACTION_BAR_FIRST_BUTTON..12,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..1,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..2,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..3,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..4
+}
+
+local SECOND_ACTION_BAR_BUTTON_NAMES = {
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..5,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..6,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..7,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..8,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..9,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..10,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..11,
+    BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON..12,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..1,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..2,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..3,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..4,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..5,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..6,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..7,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..8
+}
+
+local THIRD_ACTION_BAR_BUTTON_NAMES = {
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..9,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..10,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..11,
+    BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON..12,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..1,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..2,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..3,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..4,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..5,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..6,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..7,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..8,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..9,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..10,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..11,
+    RIGHT_ACTION_BAR_FIRST_BUTTON..12
+}
+
+local FOURTH_ACTION_BAR_BUTTON_NAMES = {
+    LEFT_ACTION_BAR_FIRST_BUTTON..1,
+    LEFT_ACTION_BAR_FIRST_BUTTON..2,
+    LEFT_ACTION_BAR_FIRST_BUTTON..3,
+    LEFT_ACTION_BAR_FIRST_BUTTON..4,
+    LEFT_ACTION_BAR_FIRST_BUTTON..5,
+    LEFT_ACTION_BAR_FIRST_BUTTON..6,
+    LEFT_ACTION_BAR_FIRST_BUTTON..7,
+    LEFT_ACTION_BAR_FIRST_BUTTON..8,
+    LEFT_ACTION_BAR_FIRST_BUTTON..9,
+    LEFT_ACTION_BAR_FIRST_BUTTON..10,
+    LEFT_ACTION_BAR_FIRST_BUTTON..11,
+    LEFT_ACTION_BAR_FIRST_BUTTON..12
+}
+
+local function RearrangeActionBarHorizontal16(names)
+    for i = 2, table.getn(names) do
+        local button = _G[names[i]]
         button:ClearAllPoints()
-        button:SetPoint("LEFT", name..i - 1, "RIGHT", 6, 0)
+        button:SetPoint("LEFT", names[i - 1], "RIGHT", 6, 0)
     end
 end
 
-local function RearrangeAllActionBarsHorizontal()
-    RearrangeActionBarHorizontal(MAIN_ACTION_BAR_FIRST_BUTTON)
-    RearrangeActionBarHorizontal(BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON)
-    RearrangeActionBarHorizontal(BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON)
-    RearrangeActionBarHorizontal(RIGHT_ACTION_BAR_FIRST_BUTTON)
-    RearrangeActionBarHorizontal(LEFT_ACTION_BAR_FIRST_BUTTON)
+local function RearrangeAllActionBarsHorizontal16()
+    RearrangeActionBarHorizontal16(FIRST_ACTION_BAR_BUTTON_NAMES)
+    RearrangeActionBarHorizontal16(SECOND_ACTION_BAR_BUTTON_NAMES)
+    RearrangeActionBarHorizontal16(THIRD_ACTION_BAR_BUTTON_NAMES)
+    RearrangeActionBarHorizontal16(FOURTH_ACTION_BAR_BUTTON_NAMES)
 end
 
 local function MoveActionBarCenterPoint(name, x, y)
-    local button = _G[name..1]
+    local button = _G[name]
     button:ClearAllPoints()
     button:SetPoint("CENTER", x, y)
+end
+
+-- I have no idea why offsetY works the way it does.
+-- Something is screwed up with what these are actually anchored to / parented from.
+local function MoveAllActionBars(offsetX, offsetY, verticalSpacing)
+    MoveActionBarCenterPoint(
+        FIRST_ACTION_BAR_BUTTON_NAMES[1],
+        -314 + offsetX,
+        613 + offsetY)
+    MoveActionBarCenterPoint(
+        SECOND_ACTION_BAR_BUTTON_NAMES[1],
+        -232 + offsetX,
+        229 + (offsetY / 2) + (-verticalSpacing))
+    MoveActionBarCenterPoint(
+        THIRD_ACTION_BAR_BUTTON_NAMES[1],
+        -650 + offsetX,
+        -85 + (-verticalSpacing * 2))
+    MoveActionBarCenterPoint(
+        FOURTH_ACTION_BAR_BUTTON_NAMES[1],
+        -1233 + offsetX,
+        -20 + offsetY + (-verticalSpacing * 3))
 end
 
 local EventFrame = CreateFrame("Frame")
@@ -79,14 +171,11 @@ EventFrame:SetScript("OnEvent", function(self, event, ...)
     RemoveBottomActionBarStyling()
     RemoveMenuMicroButtonAndBagsBar()
     RemoveMacroNamesFromAllActionBars()
-    RearrangeAllActionBarsHorizontal()
 
     CastingBarFrame:ClearAllPoints()
     CastingBarFrame:SetPoint("CENTER", 0, -130)
-
-    MoveActionBarCenterPoint(MAIN_ACTION_BAR_FIRST_BUTTON, -230, 300)
-    MoveActionBarCenterPoint(BOTTOM_RIGHT_ACTION_BAR_FIRST_BUTTON, -650, -86)
-    MoveActionBarCenterPoint(BOTTOM_LEFT_ACTION_BAR_FIRST_BUTTON, -232, 72)
-    MoveActionBarCenterPoint(RIGHT_ACTION_BAR_FIRST_BUTTON, -1276, -334)
-    MoveActionBarCenterPoint(LEFT_ACTION_BAR_FIRST_BUTTON, -1233, -376)
+    CastingBarFrame.SetPoint = function() end
+    
+    RearrangeAllActionBarsHorizontal16()
+    MoveAllActionBars(0, -300, 3)
 end)
